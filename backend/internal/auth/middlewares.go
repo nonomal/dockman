39e2e30464002b0362bfb016b0b0251de21f651e
RA341 <razerishi1505@gmail.com>
2025-06-18 13:38:27 -0400

    initial auth impl

---
+++ backend/internal/auth/middlewares.go
@@ -0,0 +1,131 @@
+package auth
+
+import (
+	"connectrpc.com/connect"
+	"context"
+	"fmt"
+	"github.com/rs/zerolog/log"
+	"net/http"
+)
+
+const HeaderAuth = "Authorization"
+const KeyUserCtx = "user"
+
+type HttpMiddleware struct {
+	Srv *Service
+}
+
+func NewHttpAuthMiddleware(srv *Service) func(next http.Handler) http.Handler {
+	return func(next http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			u, err := verifyCookie(r.Cookies(), srv)
+			if err != nil {
+				http.Error(w, err.Error(), http.StatusUnauthorized)
+				return
+			}
+
+			r.WithContext(context.WithValue(r.Context(), KeyUserCtx, u))
+			next.ServeHTTP(w, r)
+		})
+	}
+
+}
+
+func getCookie(name string, cookies []*http.Cookie) (*http.Cookie, error) {
+	if name == "" {
+		return nil, http.ErrNoCookie
+	}
+
+	for _, c := range cookies {
+		if c.Name == HeaderAuth {
+			return c, nil
+		}
+	}
+
+	return nil, http.ErrNoCookie
+}
+
+func verifyCookie(cookies []*http.Cookie, srv *Service) (*User, error) {
+	cookie, err := getCookie(HeaderAuth, cookies)
+	if err != nil {
+		return nil, err
+	}
+
+	token := cookie.Value
+	userInfo, err := srv.VerifyToken(token)
+	if err != nil {
+		log.Error().Err(err).Msg("Unable to verify token")
+		return nil, fmt.Errorf("unable to verify token")
+	}
+
+	return userInfo, nil
+}
+
+type Interceptor struct {
+	authService *Service
+}
+
+func NewInterceptor(authService *Service) *Interceptor {
+	return &Interceptor{authService: authService}
+}
+
+func (i *Interceptor) WrapStreamingHandler(next connect.StreamingHandlerFunc) connect.StreamingHandlerFunc {
+	return func(
+		ctx context.Context,
+		conn connect.StreamingHandlerConn,
+	) error {
+		ctx, err := i.verifyGrpcCookie(ctx, conn.RequestHeader())
+		if err != nil {
+			return err
+		}
+
+		return next(ctx, conn)
+	}
+}
+
+func (i *Interceptor) WrapUnary(next connect.UnaryFunc) connect.UnaryFunc {
+	return func(
+		ctx context.Context,
+		req connect.AnyRequest,
+	) (connect.AnyResponse, error) {
+		ctx, err := i.verifyGrpcCookie(ctx, req.Header())
+		if err != nil {
+			return nil, err
+		}
+
+		return next(ctx, req)
+	}
+}
+
+func (*Interceptor) WrapStreamingClient(next connect.StreamingClientFunc) connect.StreamingClientFunc {
+	return func(
+		ctx context.Context,
+		spec connect.Spec,
+	) connect.StreamingClientConn {
+		return next(ctx, spec)
+	}
+}
+
+func (i *Interceptor) verifyGrpcCookie(ctx context.Context, header http.Header) (context.Context, error) {
+	cookies, err := http.ParseCookie(header.Get("Cookie"))
+	if err != nil {
+		return ctx, err
+	}
+
+	user, err := verifyCookie(cookies, i.authService)
+	if err != nil {
+		return ctx, connect.NewError(connect.CodeUnauthenticated, err)
+	}
+
+	// add user value to subsequent requests
+	return context.WithValue(ctx, KeyUserCtx, user), nil
+}
+
+func GetUserContext(ctx context.Context) (*User, error) {
+	userVal := ctx.Value(KeyUserCtx)
+	if userVal == nil {
+		return nil, fmt.Errorf("could not find user in context")
+	}
+
+	return userVal.(*User), nil
+}
--- backend/internal/auth/model.go
+++ backend/internal/auth/model.go
@@ -1,26 +1,61 @@
 package auth
 
-//import (
-//	v1 "github.com/RA341/multipacman/generated/auth/v1"
-//	"gorm.io/gorm"
-//)
-//
-//// User storing user data for non-game uses
-//type User struct {
-//	gorm.Model
-//	Username string
-//	Password string
-//	Token    string
-//	Guest    bool
-//}
-//
-//func (user *User) FromRPC(userRpc *v1.UserResponse) {}
-//
-//func (user *User) ToRPC() *v1.UserResponse {
-//	return &v1.UserResponse{
-//		ID:        uint64(user.ID),
-//		Username:  user.Username,
-//		AuthToken: user.Token, // make sure this is un-hashed
-//		IsGuest:   user.Guest,
-//	}
-//}
+import "fmt"
+
+type User struct {
+	Username string
+	Password string
+	Token    string
+}
+
+type DB interface {
+	GetUser(username string, password string) (*User, error)
+	UpdateUser(user *User) error
+	VerifyAuthToken(token string) (*User, error)
+	NewUser(username string, password string) (*User, error)
+}
+
+type MemAuthDB struct {
+	mem map[string]*User
+}
+
+func (m *MemAuthDB) NewUser(username string, password string) (*User, error) {
+	hashedPass := hashString(password)
+
+	m.mem[username] = &User{
+		Username: username,
+		Password: hashedPass,
+		Token:    "",
+	}
+
+	return m.mem[username], nil
+}
+
+func (m *MemAuthDB) GetUser(username string, password string) (*User, error) {
+	val, ok := m.mem[username]
+	if !ok {
+		return nil, fmt.Errorf("invalid user/password")
+	}
+
+	if ok = checkPassword(password, val.Password); !ok {
+		return nil, fmt.Errorf("invalid user/password")
+	}
+
+	return val, nil
+}
+
+func (m *MemAuthDB) UpdateUser(user *User) error {
+	m.mem[user.Username] = user
+	return nil
+}
+
+func (m *MemAuthDB) VerifyAuthToken(token string) (*User, error) {
+	hashedToken := hashString(token)
+	for _, user := range m.mem {
+		if hashedToken == user.Token {
+			return user, nil
+		}
+	}
+
+	return nil, fmt.Errorf("invalid token")
+}
