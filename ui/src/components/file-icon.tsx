0395c7e7e9881941f4f9f2414722156080fdf51f
RA341 <razerishi1505@gmail.com>
2025-06-24 17:58:21 -0400

    added icons

---
+++ ui/src/components/file-icon.tsx
@@ -0,0 +1,63 @@
+import React from 'react';
+import {Description} from '@mui/icons-material';
+import {blue} from "@mui/material/colors";
+
+interface SvgFromUrlProps {
+    url: string;
+    size?: number;
+}
+
+const SvgFromUrl: React.FC<SvgFromUrlProps> = ({url, size = 25}) => {
+    return (
+        <img
+            src={url}
+            alt="SVG from URL"
+            width={size}
+            height={size}
+            style={{fill: 'currentColor'}} // Note: Styling is limited
+        />
+    );
+};
+
+const DockerComposeIcon = () => (
+    <SvgFromUrl url={'/docker.svg'}/>
+);
+
+const YamlIcon = () => (
+    <SvgFromUrl url={'/yaml.png'} />
+);
+
+const EnvIcon = () => (
+    <SvgFromUrl url={'/env.png'}/>
+);
+
+const GitignoreIcon = () => (
+    <SvgFromUrl url={'/git.svg'}/>
+);
+
+const DefaultFileIcon = () => (
+    <Description sx={{color: blue[200]}}/>
+);
+
+
+interface FileIconProps {
+    filename: string;
+}
+
+const FileIcon: React.FC<FileIconProps> = ({filename}) => {
+    if (filename.endsWith('compose.yaml')) {
+        return <DockerComposeIcon/>;
+    }
+    if (filename.endsWith('.yaml') || filename.endsWith('.yml')) {
+        return <YamlIcon/>;
+    }
+    if (filename.endsWith('.env')) {
+        return <EnvIcon/>;
+    }
+    if (filename.endsWith('.gitignore')) {
+        return <GitignoreIcon/>;
+    }
+    return <DefaultFileIcon/>;
+};
+
+export default FileIcon;
--- ui/src/components/file-item.tsx
+++ ui/src/components/file-item.tsx
@@ -1,16 +1,10 @@
 import {Link as RouterLink, useLocation} from 'react-router-dom';
 import {Collapse, IconButton, List, ListItemButton, ListItemIcon, ListItemText} from '@mui/material';
-import {
-    Add as AddIcon,
-    Article as ArticleIcon,
-    Delete as DeleteIcon,
-    Description as DescriptionIcon,
-    ExpandLess,
-    ExpandMore,
-    Folder as FolderIcon
-} from '@mui/icons-material';
+import {Add as AddIcon, Delete as DeleteIcon, ExpandLess, ExpandMore, Folder as FolderIcon} from '@mui/icons-material';
 import type {FileGroup} from "../hooks/files.ts";
 import React from "react";
+import {amber} from "@mui/material/colors";
+import FileIcon from "./file-icon.tsx";
 
 export interface FileItemProps {
     group: FileGroup;
@@ -21,78 +15,84 @@ export interface FileItemProps {
 }
 
 export const FileItem = React.memo(({group, onAdd, onDelete, isOpen, onToggle}: FileItemProps) => {
-    const location = useLocation();
-    const isDirectory = group.children.length !== 0;
+        const location = useLocation();
+        const isDirectory = group.children.length !== 0;
 
-    const handleActionClick = (e: React.MouseEvent, action: () => void) => {
-        e.preventDefault();
-        e.stopPropagation();
-        action();
-    };
+        const handleActionClick = (e: React.MouseEvent, action: () => void) => {
+            e.preventDefault();
+            e.stopPropagation();
+            action();
+        };
 
-    const handleToggle = () => {
-        if (isDirectory) {
-            onToggle(group.name);
-        }
-    };
+        const handleToggle = () => {
+            if (isDirectory) {
+                onToggle(group.name);
+            }
+        };
 
-    const ParentProps = !isDirectory ? {
-        component: RouterLink,
-        to: `/files/${group.name}?tab=editor`,
-        selected: location.pathname === `/files/${group.name}`,
-    } : {
-        onClick: handleToggle,
-    };
+        const ParentProps = !isDirectory ? {
+            component: RouterLink,
+            to: `/files/${group.name}`,
+            selected: location.pathname === `/files/${group.name}`,
+        } : {
+            onClick: handleToggle,
+        };
 
-    return (
-        <>
-            <ListItemButton {...ParentProps}>
-                <ListItemIcon>
-                    {isDirectory ? <FolderIcon/> : <DescriptionIcon/>}
-                </ListItemIcon>
-                <ListItemText primary={group.name}/>
+        return (
+            <>
+                <ListItemButton {...ParentProps}>
+                    <ListItemIcon>
+                        {isDirectory
+                            ? <FolderIcon sx={{color: amber[800]}}/>
+                            : <FileIcon filename={group.name}/>
+                        }
+                    </ListItemIcon>
+                    <ListItemText primary={group.name}/>
 
-                {isDirectory && (isOpen ? <ExpandLess/> : <ExpandMore/>)}
-                {/* Show Add button only for directories */}
-                {isDirectory && (
-                    <IconButton size="small" onClick={(e) => handleActionClick(e, () => onAdd(group.name))}>
-                        <AddIcon fontSize="small"/>
+                    {isDirectory && (isOpen ? <ExpandLess/> : <ExpandMore/>)}
+                    {isDirectory && (
+                        <IconButton size="small" onClick={(e) => handleActionClick(e, () => onAdd(group.name))}
+                                    color="success">
+                            <AddIcon fontSize="small"/>
+                        </IconButton>
+                    )}
+                    <IconButton size="small" onClick={(e) => handleActionClick(e, () => onDelete(group.name))}
+                                color="error">
+                        <DeleteIcon fontSize="small"/>
                     </IconButton>
-                )}
+                </ListItemButton>
 
-                <IconButton size="small" onClick={(e) => handleActionClick(e, () => onDelete(group.name))}>
-                    <DeleteIcon fontSize="small"/>
-                </IconButton>
-
-            </ListItemButton>
-
-            {isDirectory && (
-                <Collapse in={isOpen} timeout="auto" unmountOnExit>
-                    <List disablePadding sx={{pl: 4}}>
-                        {group.children.map((child: string) => {
-                            const childBase = `${group.name}/${child}`;
-                            const childPath = `/files/${childBase}`;
-                            return (
-                                <ListItemButton
-                                    key={child}
-                                    component={RouterLink}
-                                    to={`${childPath}?tab=editor`}
-                                    selected={location.pathname === childPath}
-                                >
-                                    <ListItemIcon><ArticleIcon sx={{fontSize: '1.25rem'}}/></ListItemIcon>
-                                    <ListItemText primary={child}/>
-                                    <IconButton
-                                        size="small"
-                                        onClick={(e) => handleActionClick(e, () => onDelete(childBase))}
+                {isDirectory && (
+                    <Collapse in={isOpen} timeout="auto" unmountOnExit>
+                        <List disablePadding sx={{pl: 4}}>
+                            {group.children.map((child: string) => {
+                                const childBase = `${group.name}/${child}`;
+                                const childPath = `/files/${childBase}`;
+                                return (
+                                    <ListItemButton
+                                        key={child}
+                                        component={RouterLink}
+                                        to={`${childPath}`}
+                                        selected={location.pathname === childPath}
                                     >
-                                        <DeleteIcon fontSize="small"/>
-                                    </IconButton>
-                                </ListItemButton>
-                            );
-                        })}
-                    </List>
-                </Collapse>
-            )}
-        </>
-    );
-});
+                                        <ListItemIcon>
+                                            <FileIcon filename={child}/>
+                                        </ListItemIcon>
+                                        <ListItemText primary={child}/>
+                                        <IconButton
+                                            size="small"
+                                            onClick={(e) => handleActionClick(e, () => onDelete(childBase))}
+                                            color="error"
+                                        >
+                                            <DeleteIcon fontSize="small"/>
+                                        </IconButton>
+                                    </ListItemButton>
+                                );
+                            })}
+                        </List>
+                    </Collapse>
+                )}
+            </>
+        );
+    }
+);
